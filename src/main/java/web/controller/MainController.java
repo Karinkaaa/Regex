25556b404472fad32b369425cb41d40b584adb2e
karin <karinka.kostraba@gmail.com>
2019-07-03 20:50:33 +0300

    configuring logging in the class MainController

--- src/main/java/web/controller/MainController.java
+++ src/main/java/web/controller/MainController.java
@@ -1,5 +1,7 @@
 package main.java.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,14 +10,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("main")
 public class MainController {
 
+    static Logger logger = LoggerFactory.getLogger("main.controller");
+
     public MainController() {
-        System.out.println("Controller created.");
+        logger.info("Controller created...");
     }
 
     @RequestMapping("get-some-data")
     @ResponseBody
     public String[] getSomeData() {
-        System.out.println("Controller method invoked.");
+        logger.info("Controller method invoked...");
         return new String[]{"1", "22", "333"};
+    }
+
+    public static void main(String[] args) {
+
+        logger.error("Main method invoked...");
     }
 }
