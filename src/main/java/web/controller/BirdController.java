5951a7fd25b9efe6a4f0c7a780b42123c68aa21c
karin <karinka.kostraba@gmail.com>
2019-07-13 17:13:23 +0300

    created controller BirdController

--- src/main/java/web/controller/BirdController.java
+++ src/main/java/web/controller/BirdController.java
@@ -1,7 +1,8 @@
 package main.java.web.controller;
 
-import main.java.web.birds.entities.Bird;
+import main.java.birds.entities.Bird;
 import main.java.web.components.BirdStoreComponent;
+import main.java.web.components.SingleBirdStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -14,12 +15,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class BirdController {
 
     private Logger logger = LoggerFactory.getLogger(BirdController.class);
-
-    private BirdStoreComponent birdStore = BirdStoreComponent.getInstance();
+    private final BirdStoreComponent birdStore = SingleBirdStore.getInstance();
 
     @RequestMapping(value = "add-bird", method = RequestMethod.PUT)
     @ResponseBody
     public String addBird(String name, String livingArea, double size) {
+
         Bird bird = new Bird(name, livingArea, size);
         birdStore.addBird(bird);
         logger.info("Adding bird...\n");
@@ -29,6 +30,7 @@ public class BirdController {
     @RequestMapping(value = "delete-bird", method = RequestMethod.DELETE)
     @ResponseBody
     public String deleteBird(String name) {
+
         birdStore.deleteBird(name);
         logger.info("Deleting bird...\n");
         return name;
@@ -37,6 +39,7 @@ public class BirdController {
     @RequestMapping(value = "update-bird", method = RequestMethod.POST)
     @ResponseBody
     public String updateBird(String name, String newName) {
+
         birdStore.updateBird(name, newName);
         logger.info("Updating bird...\n");
         return newName;
@@ -45,6 +48,7 @@ public class BirdController {
     @RequestMapping(value = "find-bird", method = RequestMethod.GET)
     @ResponseBody
     public String findBird(String name) {
+
         birdStore.searchByName(name);
         logger.info("Finding bird...\n");
         return name;
