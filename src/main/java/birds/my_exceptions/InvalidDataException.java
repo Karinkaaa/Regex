bed822749af709ab0b0224390cdc2cc0890082b1
karin <karinka.kostraba@gmail.com>
2019-07-17 19:00:49 +0300

    created own class exception

---
+++ src/main/java/birds/my_exceptions/InvalidDataException.java
@@ -0,0 +1,8 @@
+package main.java.birds.my_exceptions;
+
+public class InvalidDataException extends Exception {
+
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
