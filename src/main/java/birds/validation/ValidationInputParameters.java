609384f679971257822836c11d8857229422e90a
karin <karinka.kostraba@gmail.com>
2019-07-17 21:25:52 +0300

    implementation interface IValidationInputParameters

---
+++ src/main/java/birds/validation/ValidationInputParameters.java
@@ -0,0 +1,28 @@
+package main.java.birds.validation;
+
+import main.java.birds.my_exceptions.InvalidDataException;
+
+public class ValidationInputParameters implements IValidationInputParameters {
+
+    @Override
+    public boolean isValidStringParam(String param, int maxLength) throws InvalidDataException {
+
+        if (param.length() > maxLength) return false;
+
+        for (int i = 0; i < param.length(); i++) {
+
+            char ch = param.charAt(i);
+            if (!Character.isLetter(ch) || !(ch == ' ')) {
+                throw new InvalidDataException("Input parameter is not valid!");
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean isValidSize(double param) throws InvalidDataException {
+
+        if (param > 0.0) return true;
+        throw new InvalidDataException("Input parameter is not valid!");
+    }
+}
