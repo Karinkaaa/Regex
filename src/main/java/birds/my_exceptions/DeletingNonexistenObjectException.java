93df9e973950cd9be7b6c0911a8d27cab11b1a93
karin <karinka.kostraba@gmail.com>
2019-07-17 17:23:13 +0300

    creating own classes exception

---
+++ src/main/java/birds/my_exceptions/DeletingNonexistenObjectException.java
@@ -0,0 +1,8 @@
+package main.java.birds.my_exceptions;
+
+public class DeletingNonexistenObjectException extends Exception {
+
+    public DeletingNonexistenObjectException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/birds/my_exceptions/ExistingIdException.java
@@ -0,0 +1,8 @@
+package main.java.birds.my_exceptions;
+
+public class ExistingIdException extends Exception {
+
+    public ExistingIdException(String message) {
+        super(message);
+    }
+}
