c823e2c6ac6dbd7a45696a2f4491f2e5c5651c86
karin <karinka.kostraba@gmail.com>
2019-07-17 18:22:41 +0300

    added exception to the method signature

--- src/main/java/birds/application/BirdStoreApplication.java
+++ src/main/java/birds/application/BirdStoreApplication.java
@@ -1,11 +1,12 @@
 package main.java.birds.application;
 
+import main.java.birds.my_exceptions.ExistingIdException;
 import main.java.web.processor.IInfiniteLoopProcessor;
 import main.java.web.processor.UserCommandProcessor;
 
 public class BirdStoreApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExistingIdException {
 
         IInfiniteLoopProcessor userCommandProcessor = new UserCommandProcessor();
         userCommandProcessor.processInLoop();
